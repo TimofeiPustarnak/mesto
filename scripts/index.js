264d239e9d74f77fc78d022db72b118dd6d923d2
Timofei Pustarnak <pustimafei@gmail.com>
2020-09-07 20:26:28 +0300

    rev pt 1

--- scripts/index.js
+++ scripts/index.js
@@ -5,34 +5,33 @@ let editButton = profile.querySelector('.profile__edit-button');
 let closeButton = popup.querySelector('.popup__close-button');
 let profileTitle = profile.querySelector('.profile__title');
 let profileSubitle = profile.querySelector('.profile__subtitle');
-let popupFieldName = popup.querySelector('.popup__field_name');
-let popupFieldDescription = popup.querySelector('.popup__field_description');
+let popupFieldName = popup.querySelector('.popup__field_type_name');
+let popupFieldDescription = popup.querySelector('.popup__field_type_description');
 let popupContainer = popup.querySelector('.popup__container');
 
 popupFieldName.setAttribute('value', 'Жак-Ив Кусто');
 popupFieldDescription.setAttribute('value', 'Исследователь океана');
+profileTitle.textContent = `${popupFieldName.value}`;
+profileSubitle.textContent = `${popupFieldDescription.value}`;
 
 function openPopup() {
   popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-  editProfile();
   popup.classList.remove('popup_opened');
-}
-
-function editProfile() {
-  profileTitle.textContent = `${popupFieldName.value}`;
-  profileSubitle.textContent = `${popupFieldDescription.value}`;
+  popupFieldName.value = `${profileTitle.textContent}`;
+  popupFieldDescription.value = `${profileSubitle.textContent}`;
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
+  profileTitle.textContent = `${popupFieldName.value}`;
+  profileSubitle.textContent = `${popupFieldDescription.value}`;
   closePopup();
 }
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 popupContainer.addEventListener('submit', formSubmitHandler);
-editProfile();
 
