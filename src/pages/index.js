65a93bebb1a0360a0b7f7e5f21ccbfef5d5b195c
Timofei Pustarnak <pustimafei@gmail.com>
2021-01-19 04:35:59 +0300

    C:/Program Files/Git/

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import './pages/index.css'; 
+import './index.css'; 
 const page = document.querySelector('.page');
 const profile = page.querySelector('.profile');
 const popup = page.querySelector('#popup-person');
@@ -45,12 +45,12 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-import Card from './components/Card.js';
-import Section from './components/Section.js';
-import PopupWithImage from './components/PopupWithImage.js';
-import PopupWithForm from './components/PopupWithForm.js';
-import {FormValidator} from './components/FormValidator.js';
-import UserInfo from './components/UserInfo.js';
+import Card from '../components/Card.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import {FormValidator} from '../components/FormValidator.js';
+import UserInfo from '../components/UserInfo.js';
 
 const userInfo = new UserInfo('.profile__title', '.profile__subtitle');
 const section = new Section({
